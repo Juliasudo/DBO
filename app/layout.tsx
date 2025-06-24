a10fa4c93809d8bef6a9f2c3185f5e0de736cd10
Juliasudo <diamondkaz578@gmail.com>
2025-06-24 19:11:52 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import './styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import { ThemeProvider } from './lib/hooks/useTheme'
+import { ThemeProvider } from 'next-themes' // <-- The only change needed is here
 import Header from './sections/Header'
 import Footer from './sections/Footer'
 
